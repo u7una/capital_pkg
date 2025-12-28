b803c433f9392ed4b33fc97db4db4450303f9fa3
yuna <s24c1107ca@s.chibakoudai.jp>
2025-12-28 10:49:45 +0900

    最初のコミット

---
+++ capital_pkg/client_node.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys
+import rclpy
+from rclpy.node import Node
+from capital_pkg.srv import GetCapital
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: ros2 run capital_pkg client_node <Country>")
+        return
+
+    rclpy.init()
+    node = rclpy.create_node('capital_client')
+    client = node.create_client(GetCapital, 'get_capital')
+    
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('waiting...')
+
+    req = GetCapital.Request()
+    req.country_name = sys.argv[1]
+    
+    future = client.call_async(req)
+    rclpy.spin_until_future_complete(node, future)
+    
+    res = future.result()
+    if res.success:
+        print(f"Capital: {res.capital_name}")
+    else:
+        print("Not found")
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ capital_pkg/server_node.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+import rclpy
+from rclpy.node import Node
+from capital_pkg.srv import GetCapital # 自作型をインポート
+
+class CapitalServer(Node):
+    def __init__(self):
+        super().__init__('capital_server')
+        self.data = {"Japan": "Tokyo", "France": "Paris", "USA": "Washington D.C."}
+        self.srv = self.create_service(GetCapital, 'get_capital', self.callback)
+        self.get_logger().info('Server is Ready')
+
+    def callback(self, request, response):
+        data = {
+                "インドネシア": "ジャカルタ",
+                "フィリピン": "マニラ",
+                "シンガポール": "シンガポール",
+                "タイ": "バンコク",
+                "マレーシア": "クアラルンプール",
+                "ベトナム": "ハノイ",
+                "ラオス": "ビエンチャン",
+                "カンボジア": "プノンペン",
+                "ミャンマー": "ネピドー",
+                "ブルネイ": "バンダルスリブガワン",
+                "東ティモール": "ディリ"
+                }
+
+        country = request.country_name
+        self.get_logger().info(f'received request for: {country}')
+        if country in data:
+            response.capital_name = data[country]
+            response.success = True
+        else:
+            response.capital_name = "Not Found"
+            response.success = False
+
+        return response
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CapitalServer()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+   
